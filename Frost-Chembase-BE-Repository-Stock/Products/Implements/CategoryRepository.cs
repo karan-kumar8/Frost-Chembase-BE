3d979f8a91dc01e9e58b41f03ea4a6c1036c0f35
karan kumar <karan.kumar@skillassure.com>
2023-04-17 18:57:30 +0530

    new repo

---
+++ Frost-Chembase-BE-Repository-Stock/Products/Implements/CategoryRepository.cs
@@ -0,0 +1,43 @@
+﻿using Frost_Chembase_BE_Entities_Stock.Products;
+using Frost_Chembase_BE_Repository_Stock.Products.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frost_Chembase_BE_Repository_Stock.Products.Implements
+{
+	public class CategoryRepository : IRepository<Category>
+	{
+		public void Add(Category entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Delete(Guid id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<Category> GetAll()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Category GetById(Guid id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Category GetByName(string name)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Update(Category entity)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
